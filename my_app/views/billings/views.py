ee028e73f43a4a521eec2f07865ece5447796bd3
malik khizer <malik.khizer@rapidcompute.com>
2023-11-02 20:35:41 +0500

    working on fromend form

--- my_app/views/billings/views.py
+++ my_app/views/billings/views.py
@@ -6,7 +6,12 @@ class AirwayBillView(View):
     def get(self, request):
         # <view logic>
         services = Service.objects.all()
+        payments = Payment.objects.all()
+        shipments = ShipmentType.objects.all()
+
         context = {
-            'services': services
+            'services': services,
+            'payments':payments,
+            'shipments':shipments
         }
         return render(request,template_name='billings/list.html', context=context)
---
+++ static/custom-js/billings/billings.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    console.log("this page is loaded");
+})
