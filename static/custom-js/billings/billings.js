03857af99b2cf559ad22d000ca8f6eb57a1bc788
malik khizer <malik.khizer@rapidcompute.com>
2023-11-06 20:23:43 +0500

    plus minus icons fields done

--- static/custom-js/billings/billings.js
+++ static/custom-js/billings/billings.js
@@ -1,53 +1,21 @@
-$(document).ready(function(){
-    console.log("this page is loaded");
-})
 
-
-// Create invoice details dynamically
-// function invoiceDetails() {
-//     var div = $("<div>")
-    
-// }
-
-// function createInputRow() {
-//     var row = $('<div class="row"></div>');
-  
-//     var col1 = $('<div class="col-3"></div>');
-//     col1.append('<div class="form-group"><label for="create_id_hs_title">HS Title</label><input type="number" name="hs_title" class="form-control" id="create_id_hs_title" placeholder="Enter HS title"><div></div></div>');
-  
-//     var col2 = $('<div class="col-3"></div>');
-//     col2.append('<div class="form-group"><label for="create_id_quantity">Quantity</label><input type="number" name="quantity" class="form-control" id="create_id_quantity" placeholder="Enter Quantity"><div></div></div>');
-  
-//     var col3 = $('<div class="col-2"></div>');
-//     col3.append('<div class="form-group"><label for="create_id_price">Price</label><input type="number" name="price" class="form-control" id="create_id_price" placeholder="Enter Price"><div></div></div>');
-  
-//     var col4 = $('<div class="col-2"></div>');
-//     col4.append('<div class="form-group"><label for="create_id_total">Total</label><input type="number" name="total" class="form-control" id="create_id_total" placeholder="Enter Price"><div></div></div>');
-  
-//     var col5 = $('<div class="col-1"></div>');
-//     col5.append('<div class="mt-4 text-center"><i class="bi bi-plus-circle-fill" data-include-id="false" style="font-size: 35px;"></i></div>');
-  
-//     row.append(col1, col2, col3, col4, col5);
-//     return row;
-//   }
-
-function createPlusIcon(_class){
+function createMinusIcon(_class, _delete){
     var col_div = $('<div>')
     var col1 = col_div.clone().addClass(_class)
     var innerDiv = col_div.clone().addClass('mt-4 text-center')
-    var ITag = $("<i>").addClass('"bi bi-dash-circle-fill').css('font-size','35px')
+    var ITag = $("<i>").addClass('"bi bi-dash-circle-fill').attr('data-delete-id',_delete).css('font-size','35px')
     finalDiv = col1.append(innerDiv.append(ITag))
     return finalDiv
 }
 
-function createInputRow(_id, _name, _type, _placeholder, _class) {
+function createInputRow(_id, _name, _type, _placeholder, _class, _title,_delete) {
     // var row = $('<div class="row"></div>');
 
     var col_div = $('<div>')
-    var col1 = col_div.clone().addClass(_class)
+    var col1 = col_div.clone().addClass(_class).attr('data-delete-id',_delete)
     var innerDiv = col_div.clone().addClass('form-group')
-    var label = $("<label>").attr('for','create_id_hs_title').text('HS Title')
-    var InputField = $("<input>").attr({'name':_name, 'class':'form-control', 'id':_id, 'placeholder':_placeholder})
+    var label = $("<label>").attr('for',_id).text(_title)
+    var InputField = $("<input>").attr({'name':_name, 'type':_type, 'class':'form-control', 'id':_id, 'placeholder':_placeholder})
 
     finalDiv = col1.append(innerDiv.append(label,InputField))
     return finalDiv
@@ -56,36 +24,50 @@ function createInputRow(_id, _name, _type, _placeholder, _class) {
 
 
 $(document).on('click', '.bi-plus-circle-fill', function(){
+    var dataInvoiceDetails = $(this).attr('data-invoice-details')
     
-    var invoiceLength = $(this).closest('#invoice_details');
-    var HSTitleLength = $('#create_id_hs_title').length
-    console.log(HSTitleLength);
-    
-    if(invoiceLength.length === 1){
-        console.log('here');
-        var row1 = createInputRow(_id='create_id_hs_title', _name='hs_title', _type='number', _placeholder= 'Enter HS title',_class='col-3')
-        var row2 = createInputRow(_id='create_id_quantity', _name='quantity', _type='number', _placeholder= 'Enter Quantity',_class='col-3')
-        var row3 = createInputRow(_id='create_id_price', _name='price', _type='number', _placeholder= 'Enter Price',_class='col-2')
-        var row4 = createInputRow(_id='create_id_total', _name='total', _type='number', _placeholder= 'Enter Total',_class='col-2')
-        var PlusIcon = createPlusIcon(_class= 'col-1')
-    
-        // row = createInputRow()
-        // console.log(row);
-        $(this).closest('.row').append(row1,row2,row3,row4, PlusIcon)
+    if(dataInvoiceDetails=='true'){
+        var invoiceLength =  $(this).closest('form').find("input#create_id_hs_title").length
+        
+        if (invoiceLength< 9){
+            var row1 = createInputRow(_id='create_id_hs_title', _name='hs_title', _type='number', _placeholder= 'Enter HS title',_class='col-3 ', _title='HS Title',_delete=invoiceLength)
+            var row2 = createInputRow(_id='create_id_quantity', _name='quantity', _type='number', _placeholder= 'Enter Quantity',_class='col-3', _title='Quantity',_delete=invoiceLength)
+            var row3 = createInputRow(_id='create_id_price', _name='price', _type='number', _placeholder= 'Enter Price',_class='col-2',_title='Price',_delete=invoiceLength)
+            var row4 = createInputRow(_id='create_id_total', _name='total', _type='number', _placeholder= 'Enter Total',_class='col-2', _title='Total',_delete=invoiceLength)
+            var MinusIcon = createMinusIcon(_class= 'col-1',_delete=invoiceLength)
+            $(this).closest('.row').append(row1,row2,row3,row4, MinusIcon)
+            
+        }
+        
+        
+    } else if(dataInvoiceDetails=='false'){
+        var dimentionLength =  $(this).closest('form').find("input#create_id_length").length
+        if(dimentionLength<5){
 
+            var row1 = createInputRow(_id='create_id_length', _name='length', _type='number', _placeholder= 'Enter length',_class='col-3', _title='Length', _delete=dimentionLength)
+            var row2 = createInputRow(_id='create_id_width', _name='width', _type='number', _placeholder= 'Enter width',_class='col-3', _title='Width', _delete=dimentionLength)
+            var row3 = createInputRow(_id='create_id_height', _name='height', _type='number', _placeholder= 'Enter height',_class='col-3', _title='Height', _delete=dimentionLength)
+            var MinusIcon = createMinusIcon(_class= 'col-1', _delete=dimentionLength)
+            $(this).closest('.row').append(row1,row2,row3,row4, MinusIcon)
+        }
+        // var parentDiv = $(this).closest('.row')
+        // var divID  = parentDiv.attr('data-next-div-id');
+        // console.log("the next div id is :",divID);
+        // $("#"+divID).removeClass('d-none')
     } else{
-        var parentDiv = $(this).closest('.row')
-        var divID  = parentDiv.attr('data-next-div-id');
-        $("#"+divID).removeClass('d-none')
+        $.noop
     }
     
 
 })
 
 $(document).on('click', '.bi-dash-circle-fill', function(){
+    var deleteDivID = $(this).attr('data-delete-id')
+    var form = $(this).closest('form');
+    var divsWithSameDeleteID = form.find('div[data-delete-id="' + deleteDivID + '"]');
+    $(this).remove()
+    divsWithSameDeleteID.remove()
     
-    var parentDiv = $(this).closest('.row')
-    parentDiv.addClass('d-none')
 
 })
 
