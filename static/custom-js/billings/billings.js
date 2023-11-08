b850cec1eef65835c461df8663fdee920b3a17c0
malik khizer <malik.khizer@rapidcompute.com>
2023-11-08 20:22:43 +0500

    working on jquery

--- static/custom-js/billings/billings.js
+++ static/custom-js/billings/billings.js
@@ -35,22 +35,15 @@ $(document).on('click', '.bi-plus-circle-fill', function(){
     } else if(dataInvoiceDetails=='false'){
         var dimentionLength =  $(this).closest('form').find("input#create_id_length").length
         if(dimentionLength<5){
-
             var row1 = createInputRow(_id='create_id_length', _name='length', _type='number', _placeholder= 'Enter length',_class='col-3', _title='Length', _delete='dimension'+dimentionLength)
             var row2 = createInputRow(_id='create_id_width', _name='width', _type='number', _placeholder= 'Enter width',_class='col-3', _title='Width', _delete='dimension'+dimentionLength)
             var row3 = createInputRow(_id='create_id_height', _name='height', _type='number', _placeholder= 'Enter height',_class='col-3', _title='Height', _delete='dimension'+dimentionLength)
             var MinusIcon = createMinusIcon(_class= 'col-1', _delete='dimension'+dimentionLength)
             $(this).closest('.row').append(row1,row2,row3,row4, MinusIcon)
         }
-        // var parentDiv = $(this).closest('.row')
-        // var divID  = parentDiv.attr('data-next-div-id');
-        // console.log("the next div id is :",divID);
-        // $("#"+divID).removeClass('d-none')
     } else{
         $.noop
     }
-    
-
 })
 
 $(document).on('click', '.bi-dash-circle-fill', function(){
@@ -59,6 +52,71 @@ $(document).on('click', '.bi-dash-circle-fill', function(){
     var divsWithSameDeleteID = form.find('div[data-delete-id="' + deleteDivID + '"]');
     $(this).parent().parent().remove()
     divsWithSameDeleteID.remove()
+})
+
+
+function transformObj(json_obj) {
+    var length = json_obj.length
+    var width = json_obj.width
+    var height = json_obj.height
+
+    if (
+        length.length === width.length &&
+        length.length === height.length
+    ) {
+        console.log("ÿes same size");
+    }
+    
+}
+
+
+$("#create_billings_form_id").on('submit', function(e){
+    e.preventDefault();
+    e.stopPropagation()
+    console.log("geo g");
+
+    // var $form = $(this);
+    var formData = $(this).serializeArray();
+    // console.log("the form data is :", formData);
+    const json_obj = convertSerializerArrToJson(formData, list_fiels_names = []);
+    console.log("the json obj is :",json_obj);
+    console.log("the json obj is :",json_obj.length);
+
+
+    var length = json_obj.length
+    var width = json_obj.width
+    var height = json_obj.height
+
+    if (
+        length.length === width.length &&
+        length.length === height.length
+    ) {
+        console.log("ÿes same size");
+    }
+
+
+    console.log(json_obj.length.length);
+    
+    console.log(json_obj.width);
+
+    // var dimensions = {};
+
+    // // Assuming that the length, width, and height arrays have the same length
+    // var arrayLength = json_obj.length.length;
+
+    // for (var i = 0; i < arrayLength; i++) {
+    //     dimensions[i + 1] = {
+    //         "length": json_obj.length[i],
+    //         "width": json_obj.width[i],
+    //         "height": json_obj.height[i]
+    //     };
+    // }
+
+    // // Create the new object with the "dimensions" property
+    // var newObj = Object.assign({}, json_obj, { dimensions: dimensions });
+
+    // console.log(newObj);
+   
     
 
 })
